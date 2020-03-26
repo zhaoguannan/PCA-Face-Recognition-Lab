c731f530edaf199e85ea82c9bcbb3459b03c65ee
zhaoguannan <59362444+zhaoguannan@users.noreply.github.com>
2020-03-26 15:07:41 +0800

    Update c_iii.m

--- c_iii.m
+++ c_iii.m
@@ -8,10 +8,16 @@ R = DemeanFace' * DemeanFace;
 EV = DemeanFace * EVR;
 
 %display the eigenvectors according to the eigenvalues in descending order
-
 Temp = EV;
 for i = 1:NumOfSamples
    EV(:,i) = Temp(:, NumOfSamples + 1 - i); 
    EV(:,i) = EV(:,i)/norm(EV(:,i));
+end
+
+%display the Eigenfaces
+for i = 1 : 19
+    Display = EV(:,i);
+    Display = reshape(Display, [ImageHeight ImageWidth]);
+    figure(i), imagesc(Display), colorbar, colormap(gray), title('Eigenface');
 end
 
